fdbe80d14851f4d9a347ba81f6c8f5bd43aaad2a
qianlongo <1162633511@qq.com>
2019-08-01 17:59:30 +0800

    update:

--- 1/test.js
+++ 1/test.js
@@ -1,4 +1,18 @@
-const regex = /ab{1,5}c/g
-const string = 'abc abbc abbbc abbbbc abbbbbc abbbbbbc'
+{
 
-console.log(string.match(regex))
+  const regex = /ab{1,5}c/g
+  const string = 'abc abbc abbbc abbbbc abbbbbc abbbbbbc'
+  // 横向匹配
+  console.log(string.match(regex))
+
+}
+
+
+{
+
+  const regex = /a[123]b/g
+  const string = 'a0b a1b a2b a3b a4b a5b'
+
+  // 众向匹配
+  console.log(string.match(regex))
+}

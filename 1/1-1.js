4db1670f8957ab453d45d9f7bb824c5dfe39e370
qianlongo <1162633511@qq.com>
2019-08-16 00:15:13 +0800

    update: 正则

---
+++ 1/1-1.js
@@ -0,0 +1,10 @@
+const utils = require('../utils')
+
+utils.console('ab abb abbb', /[(ab)]{2,}/g, 'match')
+utils.console('12 123 1234 12345', /\d{2,5}?/g, 'match')
+utils.console('#ddd456', /#([0-9a-zA-Z]{6}|[0-9a-zA-Z]{3})/)
+console.log('hello'.replace(/^|$/g, '#'))
+console.log('hello'.replace(/(?=l)/g, '#'))
+console.log('hello'.replace(/(?!l)/g, '#'))
+
+console.log('123456789'.replace(/(?!^)(?=(\d{3})+$)/g, ','))

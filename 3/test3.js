0aeccfaa676338e361db08621e5e27211ec86a70
qianlongo <1162633511@qq.com>
2019-09-17 18:11:32 +0800

    update: 变成大写字母

--- 3/test3.js
+++ 3/test3.js
@@ -8,3 +8,11 @@ const trim2 = (str) => {
 
 console.log('  1223  ', '\n', trim('  1223  '))
 console.log('  1223  ', '\n', trim2('  1223  '))
+
+const titleize = (str) => {
+  return str.replace(/(?:^|\s+)\w/g, (m) => {
+    return m.toUpperCase()
+  })
+}
+
+console.log(titleize('my name is wangmin'))
